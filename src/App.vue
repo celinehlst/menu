0c547ac9feb7df3e7001e99ea18bc5f4cd052dc1
Céline <celinehilst@gmail.com>
2025-02-06 14:02:56 +0100

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import data from './data.json'
+
+const dataBase = data
+console.log(dataBase[2].author.name)
+</script>
+
+<template>
+  <header>
+    <button v-for="element in dataBase" :key="element.id">
+      {{ element.title }} ({{ element.category }})
+    </button>
+  </header>
+  <section>
+    <h1>Faites votre choix</h1>
+  </section>
+  <section v-for="element in dataBase" :key="element.id">
+    <h2>{{ element.title }}</h2>
+    <p v-if="element.synopsis">{{ element.synopsis }}</p>
+    <p v-if="element.content">{{ element.content }}</p>
+    <p v-if="element.author.name">{{ element.author.name }}</p>
+    <p v-else>{{ element.author }}</p>
+  </section>
+</template>
+
+<style scoped>
+button {
+  margin: 0px 20px;
+}
+
+section {
+  margin: 50px 0px;
+}
+</style>
