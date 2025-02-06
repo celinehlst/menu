f6d10dc387f25cf037850ef31c5fd387022b9ece
Céline <celinehilst@gmail.com>
2025-02-06 16:06:54 +0100

    length-pb

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,49 @@
 <script setup>
+import { ref, computed } from 'vue'
 import data from './data.json'
 
-const dataBase = data
-console.log(dataBase[2].author.name)
+const dataBase = ref(data)
+// console.log(dataBase[2].author.name)
+const displayMessage = ref(true)
+const displayElement = ref(false)
+
+let selection = ref({})
+
+const changeInfos = (element) => {
+  selection.value = element
+  displayMessage.value = false
+  displayElement.value = true
+}
+
+const averageRate = computed(() => {
+  let total = 0
+  for (let i = 0; i < selection.value.rates.length; i++) {
+    total += selection.value.rates[i]
+  }
+  const numOfrates = selection.value.rates.length
+  return total / numOfrates
+  // dataBase[index].rates.length
+})
 </script>
 
 <template>
   <header>
-    <button v-for="element in dataBase" :key="element.id">
+    <button v-for="element in dataBase" :key="element.id" @click="changeInfos(element)">
       {{ element.title }} ({{ element.category }})
     </button>
   </header>
   <section>
-    <h1>Faites votre choix</h1>
+    <h1 v-show="displayMessage">Faites votre choix</h1>
   </section>
-  <section v-for="element in dataBase" :key="element.id">
-    <h2>{{ element.title }}</h2>
-    <p v-if="element.synopsis">{{ element.synopsis }}</p>
-    <p v-if="element.content">{{ element.content }}</p>
-    <p v-if="element.author.name">{{ element.author.name }}</p>
-    <p v-else>{{ element.author }}</p>
+  <section class="content" v-show="displayElement">
+    <h2>{{ selection.title }}</h2>
+    <p>{{ selection.synopsis }}</p>
+    <p>{{ selection.content }}</p>
+    <!-- <p v-if="(selection.category === 'article') & selection.author">{{ selection.author.name }}</p> -->
+    <p v-if="selection.category === 'livre'">{{ selection.author }}</p>
+    <!-- <p v-else>{{ selection.author }}</p> -->
+    <p>Pas encore de like {{ selection.rates }}</p>
+    <p>Pas encore de like {{ averageRate }}</p>
   </section>
 </template>
 
@@ -30,5 +54,11 @@ button {
 
 section {
   margin: 50px 0px;
+}
+
+.content {
+  background: rgb(233, 197, 121);
+  border-radius: 20px;
+  padding: 10px;
 }
 </style>
